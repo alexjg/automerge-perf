cdee6b0e6e1f0375e85d2c76806ecc08dd8ca4d6
Alex Good <alex@memoryandthought.me>
2020-03-31 14:50:27 +0100

    Add some scripting

--- automerge-text.js
+++ automerge-text.js
@@ -1,6 +1,15 @@
 // Apply the paper editing trace to an Automerge.Text object, one char at a time
 const { edits, finalText } = require('./editing-trace')
-const Automerge = require('../automerge/src/automerge')
+//const AUTOMERGE_PACKAGE_LOCATION = process.env["AUTOMERGE_PACKAGE_LOCATION"]
+const AUTOMERGE_PACKAGE_LOCATION = "/home/alex/Projects/open-source/automerge/src/automerge"
+const Automerge = require(AUTOMERGE_PACKAGE_LOCATION)
+if (process.argv[2] === "wasm"){
+    const WASM_PATH = "/home/alex/Projects/automerge-stuff/automerge-rs/automerge-backend-wasm"
+    //const WASM_PATH = process.env.WASM_BACKEND_PATH
+    if (!WASM_PATH) throw "Undefined WASM_BACKEND_PATH"
+    const Backend = require(WASM_PATH)
+    Automerge.setDefaultBackend(Backend)
+}
 
 const start = new Date()
 let state = Automerge.from({text: new Automerge.Text()})
